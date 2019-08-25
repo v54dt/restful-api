eb95fcf14777ef71de541e5092a039d86d70e2c6
v54dt <vincent71497867@gmail.com>
2019-08-25 23:05:46 +0800

    Create mongodb.js

---
+++ mongodb.js
@@ -0,0 +1,24 @@
+/*var mongoose = require('mongoose');
+var config = require('./config');
+
+mongoose.connect(config.db_path);
+mongoose.Promise = global.Promise;
+
+var mongodb = mongoose.connection;
+mongodb.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+module.exports = {
+    mongodb
+};*/
+
+var config = require('./config');
+var MongoClient = require('mongodb').MongoClient;
+var url = db_path;
+
+MongoClient.connect(url,function(err,db){
+    var db_read = db.db("Test0702");
+    db_read.collection("0716").findOne({},function(err,result){
+        
+    })
+
+})
