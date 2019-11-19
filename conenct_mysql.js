51b1a121435905acdae8dd3ccf446a93fdbb6007
v54dt <vincent71497867@gmail.com>
2019-11-19 19:50:55 +0800

    Delete Swagger code

--- conenct_mysql.js
+++ conenct_mysql.js
@@ -1,23 +1,52 @@
 var mysql = require('mysql');
-
+var fs = require('fs');
 var con = mysql.createConnection({
-    host: "",
-    user: "root",
-    password: "root"
+  host: "173.194.106.188",
+  user: "root",
+  password: "root"
 });
 
 con.connect(function (err) {
-    if (err) throw err;
-    //console.log("Connected!");
-    
-    con.query("SELECT * FROM ecg.ecgdata12 limit 10 Offset 10000000", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        // offset 10000000  = > 1566875030 (2019-08-27)
-     
-    });
+  if (err) throw err;
+  //console.log("Connected!");
 
-    con.end();
+  con.query("SELECT * FROM ecg.ecgdata12 limit 10 Offset 10", function (err, result, fields) {
+    if (err) throw err;
+    //console.log(result);
+    // offset 10000000  = > 1566875030 (2019-08-27)
+
+    var output_list = [];
+
+    for (var i = 0; i < result.length; i++) {
+      var tmp = {
+        id: result[i].id,
+        timestamp: result[i].timestamp,
+        L1: result[i].L1,
+        L2: result[i].L2,
+        L3: result[i].L3,
+        v1: result[i].v1,
+        v2: result[i].v2,
+        v3: result[i].v3,
+        aVR: result[i].aVR,
+        aVL: result[i].aVL,
+        aVF: result[i].aVF,
+        userId: result[i].userId
+
+
+      }
+      //console.log(result[i].L1);
+      output_list.push(tmp);
+    }
+
+    fs.appendFile('test.txt', output_list, function (err, res) {
+      if (err) throw err;
+      console.log(output_list);
+    })
+
+    //if (err) throw err;
+  });
+  con.end();
+
 });
 
 
