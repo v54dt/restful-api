c74dc13e9ddbd1e29b08f843a5e44b63fbdc5dbd
v54dt <vincent71497867@gmail.com>
2019-12-12 12:25:36 +0800

    bug fixes and rename RPN

--- set_env/Insert_MongoDB_Sensor.js
+++ set_env/Insert_MongoDB_Sensor.js
@@ -8,31 +8,34 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db){
     var db_write = db.db("TestServer");
 
     var mongo_obj1={
-        device_id : 18,
-        ble_name: "ECG-XXXXXXXX",
-        BATT : 73
+        Device_ID : "AABBCCDD0011",
+        BLE_Name: "ECG-XXXXXXXX",
+        BATT : 73,
+        Status : 2
     }
 
-    db_write.collection("Patient").insertOne(mongo_obj1);
+    db_write.collection("Sensor").insertOne(mongo_obj1);
     
     var mongo_obj2={
-        device_id : 19,
-        ble_name: "ECG-test1",
-        BATT : 73
+        Device_ID : "AABBCCDD0022",
+        BLE_Name: "ECG-test1",
+        BATT : 73,
+        Status : 2
     }
 
 
 
 
-    db_write.collection("Patient").insertOne(mongo_obj2);
+    db_write.collection("Sensor").insertOne(mongo_obj2);
     
     var mongo_obj3={
-        device_id : 20,
-        ble_name: "ECG-test2",
-        BATT : 73
+        Device_ID : "AABBCCDD00233",
+        BLE_Name: "ECG-test2",
+        BATT : 73,
+        Status : 2
     }
 
-    db_write.collection("Patient").insertOne(mongo_obj3);
+    db_write.collection("Sensor").insertOne(mongo_obj3);
 
     db.close();
 
