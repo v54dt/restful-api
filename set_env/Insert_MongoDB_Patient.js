b09ffbbb71d080336a1416c328188ffae97d38f9
v54dt <vincent71497867@gmail.com>
2019-12-14 18:26:40 +0800

    Update Insert_MongoDB_Patient.js

--- set_env/Insert_MongoDB_Patient.js
+++ set_env/Insert_MongoDB_Patient.js
@@ -11,7 +11,7 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db){
         MRN : 12345678,
         Patient_Name: "柯博文",
         Room_ID: "702",
-        Bed_ID: "1"
+        Bed_ID: "702-1"
     }
 
     db_write.collection("Patient").insertOne(mongo_obj1);
@@ -21,7 +21,7 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db){
         MRN : 1576323,
         Patient_Name: "王阿明",
         Room_ID: "503",
-        Bed_ID: "3"
+        Bed_ID: "503-3"
     }
 
     db_write.collection("Patient").insertOne(mongo_obj2);
@@ -30,7 +30,7 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db){
         MRN : 15233678,
         Patient_Name: "李阿哈",
         Room_ID: "815",
-        Bed_ID: "2"
+        Bed_ID: "815-2"
     }
 
     db_write.collection("Patient").insertOne(mongo_obj3);
@@ -39,7 +39,7 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db){
         MRN : 8534,
         Patient_Name: "賈伯斯",
         Room_ID: "331",
-        Bed_ID: "4"
+        Bed_ID: "331-4"
     }
 
     db_write.collection("Patient").insertOne(mongo_obj4);
